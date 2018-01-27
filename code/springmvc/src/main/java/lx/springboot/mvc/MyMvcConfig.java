763ae4208b8cbb171bfa154d26626ab3daf8fa91
lx <sighof001@163.com>
2018-01-27 12:40:19 +0800

    add interceptor

--- code/springmvc/src/main/java/lx/springboot/mvc/MyMvcConfig.java
+++ code/springmvc/src/main/java/lx/springboot/mvc/MyMvcConfig.java
@@ -4,10 +4,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
+
+import lx.springboot.mvc.interceptor.DemoInterceptor;
+import lx.springboot.mvc.interceptor.DemoInterceptor2;
 
 @Configuration
 @ComponentScan("lx.springboot.mvc")
@@ -23,8 +27,24 @@ public class MyMvcConfig extends WebMvcConfigurerAdapter{
 		return r;
 	}
 	
+	@Bean
+	public DemoInterceptor demoInterceptor(){
+		return new DemoInterceptor();
+	}
+	
+	@Bean
+	public DemoInterceptor2 demoInterceptor2(){
+		return new DemoInterceptor2();
+	}
+	
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry.addResourceHandler("/assets/**").addResourceLocations("classpath:/assets/");
+	}
+	
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(demoInterceptor());
+		registry.addInterceptor(demoInterceptor2());
 	}
 }
---
+++ code/springmvc/src/main/java/lx/springboot/mvc/interceptor/DemoInterceptor2.java
@@ -0,0 +1,32 @@
+package lx.springboot.mvc.interceptor;
+
+import java.util.UUID;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+public class DemoInterceptor2 extends HandlerInterceptorAdapter{
+	private String reqId;
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+		long startTime = System.currentTimeMillis();
+		String reqId = UUID.randomUUID().toString();
+		this.reqId = reqId;
+		System.out.println("DemoInterceptor2 pre,reqId:" + reqId + ",startTime:" + startTime);
+		request.setAttribute("startTime" + reqId, startTime);
+		
+		return true;
+	}
+	
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,ModelAndView modelAndView) throws Exception {
+		long startTime = (Long)request.getAttribute("startTime" + this.reqId);
+		request.removeAttribute("startTime" + this.reqId);
+		long endTime = System.currentTimeMillis();
+		long costTime = endTime - startTime;
+		System.out.println("DemoInterceptor2 post,reqId:" + this.reqId + ",costTime:" + costTime);
+	}
+}
