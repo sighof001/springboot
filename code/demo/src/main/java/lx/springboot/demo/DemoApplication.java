c2bbcdbb79fc51366fe7404d70fd38ce7d377547
lx <sighof001@163.com>
2018-01-30 10:39:12 +0800

    ok

--- code/demo/src/main/java/lx/springboot/demo/DemoApplication.java
+++ code/demo/src/main/java/lx/springboot/demo/DemoApplication.java
@@ -1,5 +1,6 @@
 package lx.springboot.demo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,9 +9,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @SpringBootApplication
 public class DemoApplication {
+	@Value("${aaa}")
+	private String id;
+	
+	@Value("${bbb}")
+	private String name;
+	
 	@RequestMapping("/")
 	public String index() {
-		return "hello spring boot";
+		return "hello spring boot,id:" + id + ",name:" + name;
 	}
 	
 	public static void main(String[] args) {
